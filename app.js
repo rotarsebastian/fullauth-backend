6f02cd7305cb931130c8d9157bd2ef1e5c164260
Sebastian Rotar <rotar.seby1@gmail.com>
2020-04-27 13:21:31 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    return res.sendFile(__dirname + '/public/staticServer.html');
+})
+
+const ssrPage = fs.readFileSync('public/serverSideRendered.html', 'utf8');
+
+app.get('/ssr', (req, res) => {
+    return res.send(ssrPage);
+})
+
+const PORT = process.env.PORT || 8088;
+
+// #############################################
+app.listen(PORT, (err, res) => {
+    return err ? console.log('Server error') : console.log('Server listening on port ' + PORT);
+});
